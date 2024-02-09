dfd546bc5cd70441053d973e89d27106764db20f
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2024-02-09 21:48:58 +0200

    add function getAllContacts

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,6 +1,11 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+// import HttpError from "../helpers/HttpError.js";
+
+export const getAllContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.json(result);
+};
 
 export const getOneContact = (req, res) => {};
 
@@ -9,3 +14,7 @@ export const deleteContact = (req, res) => {};
 export const createContact = (req, res) => {};
 
 export const updateContact = (req, res) => {};
+
+export default {
+  getAllContacts,
+};
