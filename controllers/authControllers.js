bbcfcac2a0620d307cacf3c665e892d9d6fc9b86
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2024-02-22 17:24:34 +0200

    add findUser

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -4,6 +4,12 @@ import HttpError from "../helpers/HttpError.js";
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
 
 const signup = async (req, res) => {
+  const { email } = req.body;
+  const user = await authServices.findUser({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+
   const newUser = await authServices.signup(req.body);
   res.status(201).json({
     email: newUser.email,
