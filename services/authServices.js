854a5bde9a0ec55fbc2ce9dc70fd3eac7fb77b6c
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2024-02-22 17:31:22 +0200

    add hashPassword

--- services/authServices.js
+++ services/authServices.js
@@ -1,5 +1,12 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 export const findUser = (filter) => User.findOne(filter);
 
-export const signup = (data) => User.create(data);
+// export const signup = (data) => User.create(data);
+
+export const signup = async (data) => {
+  const { password } = data;
+  const hashPassword = await bcrypt.hash(password, 10);
+  return User.create({ ...data, password: hashPassword });
+};
