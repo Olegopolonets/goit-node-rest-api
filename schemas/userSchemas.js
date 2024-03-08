542ed9c355a512f1a5873e5a8c0640cfce035660
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2024-03-08 16:11:27 +0200

    add verifySchema

--- schemas/userSchemas.js
+++ schemas/userSchemas.js
@@ -1,4 +1,5 @@
 import Joi from "joi";
+import { emailRegexp } from "../constants/regexp.js";
 
 export const signupSchema = Joi.object({
   password: Joi.string().min(6).required().messages({
@@ -24,4 +25,8 @@ export const signinSchema = Joi.object({
   subscription: Joi.string().valid("starter", "pro", "business").messages({
     "any.only": "Subscription has only 3 values: starter, pro, business",
   }),
+});
+
+export const verifySchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
 });
